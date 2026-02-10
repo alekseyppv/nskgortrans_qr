754322b58d1c9d10b14edbe1e417047521f51aae
Aleksey Popov <77558665+alekseyppv@users.noreply.github.com>
2026-02-10 12:39:47 +0700

    Update init.py

--- custom_components/nskgortrans_qr/init.py
+++ custom_components/nskgortrans_qr/init.py
@@ -14,3 +14,13 @@ async def async_setup_entry(hass, entry):
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+async def async_reload_entry(hass, entry):
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
+
+
+async def async_unload_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
