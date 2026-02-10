9813e10c8d6e8aeefd13ff022863484bbb7e6429
Aleksey Popov <77558665+alekseyppv@users.noreply.github.com>
2026-02-10 12:20:43 +0700

    Update config_flow.py

--- custom_components/nskgortrans_qr/config_flow.py
+++ custom_components/nskgortrans_qr/config_flow.py
@@ -2,6 +2,7 @@ import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN, TRANSPORT_TYPES
 
+
 class NSKgortransQrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
@@ -12,20 +13,22 @@ class NSKgortransQrConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 data=user_input,
             )
 
-        schema = vol.Schema({
-            vol.Required("url"): str,
-            vol.Required("scan_interval", default=60): vol.All(
-                vol.Coerce(int), vol.Range(min=30, max=600)
-            ),
-            vol.Required("routes"): [
-                {
-                    vol.Required("number"): str,
-                    vol.Required("type"): vol.In(TRANSPORT_TYPES),
-                }
-            ],
+        data_schema = vol.Schema(
+            {
+                vol.Required("url"): str,
+                vol.Required("scan_interval", default=60): vol.All(
+                    vol.Coerce(int), vol.Range(min=30, max=600)
+                ),
+                vol.Required("routes"): [
+                    {
+                        vol.Required("number"): str,
+                        vol.Required("type"): vol.In(TRANSPORT_TYPES),
+                    }
+                ],
+            }
         )
 
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=data_schema,
         )
