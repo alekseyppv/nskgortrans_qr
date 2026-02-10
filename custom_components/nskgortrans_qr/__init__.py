dc2d8642e6559d3b9a1b1c10f85da1bec679e3e2
Aleksey Popov <77558665+alekseyppv@users.noreply.github.com>
2026-02-10 14:36:36 +0700

    Create __init__.py

---
+++ custom_components/nskgortrans_qr/__init__.py
@@ -0,0 +1,23 @@
+from .const import DOMAIN
+from .coordinator import NSKCoordinator
+
+
+async def async_setup_entry(hass, entry):
+    coordinator = NSKCoordinator(
+        hass,
+        entry.data["url"],
+        entry.data["scan_interval"],
+    )
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
